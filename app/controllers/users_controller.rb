3bda56e4123da57cf8b806831e559a66fe7b7ac8
shwan01 <tenori6452@gmail.com>
2017-10-10 19:06:50 +0900

    edit users_controller and view files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to controller: 'chats', action: 'index'
+    end
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :password)
+  end
 end
