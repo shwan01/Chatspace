bf1a886f30c433f502e15b2fb3cc3f7ace507d4f
shwan01 <tenori6452@gmail.com>
2017-10-11 15:51:20 +0900

    link to sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
   root 'chats#index'
   resources :users, only: [:edit, :update]
 end
