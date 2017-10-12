9a3cbe3fac43ce9e34bb89ece29fa4b4d510c13e
shwan01 <tenori6452@gmail.com>
2017-10-12 14:42:35 +0900

    associate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
+  has_many :groups, through :members
+  has_many :members
 end
