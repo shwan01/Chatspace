f7d474468886c8f569bb0a2fdb87495e4820e62b
shwan01 <tenori6452@gmail.com>
2017-10-19 14:39:15 +0900

    チャット送信時に文字か画像どちらか入力必須

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,5 +2,12 @@ class Chat < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
+  validates :message_or_image, presence: true
+
   mount_uploader :image, ImageUploader
+
+  private
+    def message_or_image
+      message.presence or image.presence
+    end
 end
