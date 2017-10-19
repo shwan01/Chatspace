9a876484fddbb5623dbcc34c85519e6c544d1510
shwan01 <tenori6452@gmail.com>
2017-10-19 12:36:50 +0900

    画像送信機能の実装

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,8 @@
 class ChatsController < ApplicationController
   def index
     @groups = current_user.groups
+    @group = Group.find(params[:group_id])
+    @chat = Chat.new
   end
 
   def new
@@ -9,12 +11,19 @@ class ChatsController < ApplicationController
 
   def create
     @chat = Chat.new(chat_params)
+    binding.pry
+    if @chat.save
+      redirect_to group_chats_path, notice: 'チャットを送信しました'
+    else
+      redirect_to group_chats_path, alert: 'チャット送信に失敗しました'
+    end
   end
 
   private
     def chat_params
-      params.require(:chat).permit(
-        :message
+      params.require(:chat).permit(:message, :image).merge(
+        user_id: current_user.id,
+        group_id: params[:group_id]
         )
     end
 end
