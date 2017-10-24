b4d6601328bac6b23273c211f4d59986a52268f9
shwan01 <tenori6452@gmail.com>
2017-10-24 15:05:18 +0900

    Fakerで記述を変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :user do
     name                  Faker::Name.first_name
     email                 Faker::Internet.email
-    password              "00000000"
-    password_confirmation "00000000"
+    password              Faker::Internet.password(8)
   end
 end
