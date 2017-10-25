e7f4168a1188829f9d87174ff2b24f43cbead135
shwan01 <tenori6452@gmail.com>
2017-10-25 16:23:06 +0900

    画像に対応

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,14 +1,27 @@
 $(function(){
   function buildHTML(chat){
-    var html = `<div class="content__chats__top">
+    console.log(chat.image)
+    if(chat.image.url == null){
+      var html =`<div class="content__chats__top">
                   <div class="content__chats__top__name">${chat.user_name}</div>
                   <div class="content__chats__top__time">${chat.created_at}</div>
                 </div>
                 <div class="content__chats__message">${chat.message}
                  </div>`
+    } else {
+      var html = `<div class="content__chats__top">
+                    <div class="content__chats__top__name">${chat.user_name}</div>
+                    <div class="content__chats__top__time">${chat.created_at}</div>
+                  </div>
+                  <div class="content__chats__message">${chat.message}
+                   </div>
+                  <div class="content__chats__image">
+                    <img src="${chat.image.url}">
+                  </div>`
+    }
     return html;
   }
-
+  // <img src="${chat.image}">
   $('#new_chat').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -24,10 +37,11 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.content__chats').append(html)
-      $('field').val('')
+      $('.field').val('')
     })
     .fail(function(){
       alert('error');
     })
+    $("html, body").animate({scrollTop:$(document).height()});
   })
 })
