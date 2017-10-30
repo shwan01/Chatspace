d5400579b6676c2def37f0db55466a8d585c033f
shwan01 <tenori6452@gmail.com>
2017-10-30 20:34:10 +0900

    jqueryで自動更新

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,15 +1,16 @@
 $(function(){
   function buildHTML(chat){
-    console.log(chat.image)
     if(chat.image.url == null){
-      var html =`<div class="content__chats__top">
+      var html =`<div class="chat" data-chat-id=${chat.id}>
+                <div class="content__chats__top">
                   <div class="content__chats__top__name">${chat.user_name}</div>
                   <div class="content__chats__top__time">${chat.created_at}</div>
                 </div>
                 <div class="content__chats__message">${chat.message}
-                 </div>`
+                 </div></div>`
     } else {
-      var html = `<div class="content__chats__top">
+      var html = `<div class="chat data-chat-id=${chat.id}">
+                  <div class="content__chats__top">
                     <div class="content__chats__top__name">${chat.user_name}</div>
                     <div class="content__chats__top__time">${chat.created_at}</div>
                   </div>
@@ -17,7 +18,7 @@ $(function(){
                    </div>
                   <div class="content__chats__image">
                     <img src="${chat.image.url}">
-                  </div>`
+                  </div></div>`
     }
     return html;
   }
@@ -45,3 +46,58 @@ $(function(){
     $("html, body").animate({scrollTop:$(document).height()});
   })
 })
+
+$(function(){
+  function loadHTML(chat){
+    if(chat.image.url == null){
+      var html =`<div class="chat" data-chat-id=${chat.id}>
+                  <div class="content__chats__top">
+                  <div class="content__chats__top__name">${chat.user_name}</div>
+                  <div class="content__chats__top__time">${chat.created_at}</div>
+                </div>
+                <div class="content__chats__message">${chat.message}
+                 </div></div>`
+    } else {
+      var html = `<div class="chat" data-chat-id=${chat.id}>
+                  <div class="content__chats__top">
+                    <div class="content__chats__top__name">${chat.user_name}</div>
+                    <div class="content__chats__top__time">${chat.created_at}</div>
+                  </div>
+                  <div class="content__chats__message">${chat.message}
+                   </div>
+                  <div class="content__chats__image">
+                    <img src="${chat.image.url}">
+                  </div></div>`
+    }
+    return html;
+  }
+  $(window).on('load', function(){
+    var interval = setInterval(function(){
+      if (window.location.href.match(/\/groups\/\d+\/chats/)) {
+    var chat_id = $('.chat:last').data('chat-id');
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      dataType : 'json'
+    })
+    .done(function(chat) {
+      // var id = $('.chat').data('chat-id');
+      // console.log(id)
+      var insertHTML = '';
+      // $.each(json, function(i, chat) {
+      chat.forEach(function(c){
+        if (c.id > chat_id){
+          insertHTML += loadHTML(c);
+        }
+      });
+      $('.content__chats').append(insertHTML);
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    });
+    $("html, body").animate({scrollTop:$(document).height()});
+  } else {
+    clearInterval(interval);
+   }} , 5 * 1000 );
+  });
+});
--- app/views/chats/create.json.jbuilder
+++ app/views/chats/create.json.jbuilder
@@ -4,3 +4,4 @@ json.group_id  @chat.group_id
 json.user_name  @chat.user.name
 json.user_id  @chat.user_id
 json.created_at  @chat.created_at
+json.id @chat.id
